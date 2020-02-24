f661b18f57199a87a1e7ead1b034590a0f3c67e9
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-02-24 18:14:44 +0200

    Uploading 2

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	var feedbackSlider = $('.feedback');
+
+	feedbackSlider.slick({
+		slidesToShow: 1,
+		autoplays: true,
+		dots: true
+	});
+       
+    
+
+});
+
