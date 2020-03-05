48cec4360dab69f117f8e24178b5ca711fb562c3
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-03-06 00:50:18 +0200

    Reload

--- js/common.js
+++ js/common.js
@@ -1,14 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var feedbackSlider = $('.feedback');
+    $('.tab-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '200px',
+        responsive: [{
 
-	feedbackSlider.slick({
-		slidesToShow: 1,
-		autoplays: true,
-		dots: true
-	});
-       
-    
+            breakpoint: 991,
+            settings: {
+                centerPadding: '64px'
+            },
+
+            breakpoint: 766,
+            settings: {
+                centerPadding: '40px'
+            },
+
+            breakpoint: 480,
+            settings: {
+                centerPadding: '4px'
+            },
+
+        }]
+
+    });
+
+    jQuery('.banner__row').each(function() {
+        jQuery(this).find('a').each(function(i) {
+
+            jQuery(this).click(function(e){
+                e.preventDefault();
+                jQuery(this).addClass('toggle-list__item_active').siblings().removeClass('toggle-list__item_active')
+                    .parents('body').find('.tab').eq(i).addClass('tab_visible').fadeIn(900).siblings('.tab').removeClass('tab_visible');
+                           
+
+            });
+        });
+    });
 
 });
-
